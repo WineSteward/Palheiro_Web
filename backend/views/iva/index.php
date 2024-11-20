ceabb8793a1e869e75455d043b1f1ed2ddb5fa8b
WineSteward <miguel2001carpinteiro@hotmail.com>
2024-11-20 12:24:00 +0000

    ivas CRUD and presentation

---
+++ backend/views/iva/index.php
@@ -0,0 +1,43 @@
+<?php
+
+use common\models\Iva;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use yii\helpers\Html;
+use yii\helpers\Url;
+
+/** @var yii\web\View $this */
+/** @var \backend\models\IvaSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Ivas';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="iva-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Iva', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'valorPorcentagem',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Iva $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
