c9fabc85d8df16bb1eb9d1a19e81c33a74b6fb56
WineSteward <miguel2001carpinteiro@hotmail.com>
2025-01-03 16:50:28 +0000

    improved navbar

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -17,6 +17,7 @@ AppAsset::register($this);
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
 <html lang="<?= Yii::$app->language ?>" class="h-100">
+
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -31,179 +32,218 @@ AppAsset::register($this);
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;600;900&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    
+
 </head>
+
 <body class="d-flex flex-column h-100">
-<?php $this->beginBody() ?>
+    <?php $this->beginBody() ?>
 
-<!-- Page Preloder -->
-<div id="preloder">
-    <div class="loader-container">
-        <?= Html::img(UrlHelper::getCompanyImageUrl('palheiroLogo.png'))?>
-        <div class="loader"></div>
+    <!-- Page Preloder -->
+    <div id="preloder">
+        <div class="loader-container">
+            <?= Html::img(UrlHelper::getCompanyImageUrl('palheiroLogo.png')) ?>
+            <div class="loader"></div>
+        </div>
     </div>
-</div>
 
-<header>
+    <header>
 
+        <?php
+        NavBar::begin([
+            'brandLabel' => Html::img(
+                UrlHelper::getCompanyImageUrl(urlencode('palheiroLogo.png')),
+                ['alt' => Yii::$app->name, 'style' => 'max-width: 40px; height: auto;']
+            ),
+            'brandUrl' => Yii::$app->homeUrl,
+            'options' => [
+                'class' => 'navbar navbar-expand-md navbar-light bg-light fixed-top',
+            ],
+        ]);
+
+        $menuItems = [
+            [
+                'label' => 'Home',
+                'url' => ['/site/index'],
+                'options' => ['class' => Yii::$app->controller->id === 'site' ? 'active' : ''],
+            ],
+            [
+                'label' => 'Produtos',
+                'url' => ['/produto/index'],
+                'options' => ['class' => Yii::$app->controller->id === 'produto' ? 'active' : ''],
+            ],
+            [
+                'label' => 'Contactos',
+                'url' => ['/contact/index'],
+                'options' => ['class' => Yii::$app->controller->id === 'contact' ? 'active' : ''],
+            ],
+            [
+                'label' => 'Carrinho',
+                'url' => ['/carrinho/index'],
+                'options' => ['class' => Yii::$app->controller->id === 'carrinho' ? 'active' : ''],
+            ],
+        ];
+
+        if (!Yii::$app->user->isGuest) {
+            $menuItems[] = [
+                'label' => 'Faturas',
+                'url' => ['/fatura/index'],
+                'options' => ['class' => Yii::$app->controller->id === 'fatura' ? 'active' : ''],
+            ];
+        }
+
+        echo Nav::widget([
+            'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
+            'items' => $menuItems,
+        ]);
+
+        if (Yii::$app->user->isGuest) {
+            echo Html::tag(
+                'div',
+                Html::a(
+                    'Login',
+                    ['/site/login'],
+                    ['class' => ['btn btn-link login text-dark text-decoration-none fa fa-user']]
+                ),
+                ['class' => ['d-flex']]
+            );
+            echo Html::tag(
+                'div',
+                Html::a(
+                    'Signup',
+                    ['/site/signup'],
+                    ['class' => ['btn btn-link login text-dark text-decoration-none fa']]
+                ),
+                ['class' => ['d-flex']]
+            );
+        } else {
+            echo Html::a(
+                '<i class="fa fa-user"></i> Perfil',
+                ['/profile/index'],
+                ['class' => 'btn text-decoration-none'],
+            );
+            echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
+                . Html::submitButton(
+                    'Logout (' . Yii::$app->user->identity->username . ')',
+                    [
+                        'class' => 'btn btn-link logout text-decoration-none text-dark',
+                        'style' => 'margin-top: -3px;'
+                    ]
+                )
+                . Html::endForm();
+        }
+        
+
+        NavBar::end();
+        ?>
+    </header>
+
+    <main role="main" class="flex-shrink-0">
+        <div class="container">
+            <?= Breadcrumbs::widget([
+                'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+            ]) ?>
+            <?= Alert::widget() ?>
+            <?= $content ?>
+        </div>
+    </main>
+
+    <!-- Footer Section Begin -->
+    <footer class="footer spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 col-sm-6">
+                    <div class="footer__about">
+                        <div class="footer__about__logo">
+                            <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                        </div>
+                        <ul>
+                            <li>Address: 60-49 Road 11378 New York</li>
+                            <li>Phone: +65 11.188.888</li>
+                            <li>Email: hello@colorlib.com</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-6 offset-lg-1">
+                    <div class="footer__widget">
+                        <h6>Useful Links</h6>
+                        <ul>
+                            <li><a href="#">About Us</a></li>
+                            <li><a href="#">About Our Shop</a></li>
+                            <li><a href="#">Secure Shopping</a></li>
+                            <li><a href="#">Delivery infomation</a></li>
+                            <li><a href="#">Privacy Policy</a></li>
+                            <li><a href="#">Our Sitemap</a></li>
+                        </ul>
+                        <ul>
+                            <li><a href="#">Who We Are</a></li>
+                            <li><a href="#">Our Services</a></li>
+                            <li><a href="#">Projects</a></li>
+                            <li><a href="#">Contact</a></li>
+                            <li><a href="#">Innovation</a></li>
+                            <li><a href="#">Testimonials</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-12">
+                    <div class="footer__widget">
+                        <h6>Join Our Newsletter Now</h6>
+                        <p>Get E-mail updates about our latest shop and special offers.</p>
+                        <form action="#">
+                            <input type="text" placeholder="Enter your mail">
+                            <button type="submit" class="site-btn">Subscribe</button>
+                        </form>
+                        <div class="footer__widget__social">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-instagram"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                            <a href="#"><i class="fa fa-pinterest"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer__copyright">
+                        <div class="footer__copyright__text">
+                            <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+                                Copyright &copy;<script>
+                                    document.write(new Date().getFullYear());
+                                </script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
+                        </div>
+                        <div class="footer__copyright__payment"><img src="img/payment-item.png" alt=""></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
+            <p class="float-end"><?= Yii::powered() ?></p>
+        </div>
+    </footer>
+    <!-- Footer Section End -->
+
+    <!-- Floating Cart Button -->
     <?php
-    NavBar::begin([
-        'brandLabel' => Html::img(UrlHelper::getCompanyImageUrl(urlencode('palheiroLogo.png')), ['alt' => Yii::$app->name,'style' => 'max-width: 40px; height: auto;']),
-        'brandUrl' => Yii::$app->homeUrl,
-        'options' => [
-            'class' => 'navbar navbar-expand-md navbar-light bg-light fixed-top',
-        ],
-    ]);
-    $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'Produtos', 'url' => ['/produto/index']],
-        ['label' => 'Contactos', 'url' => ['/contact/index']],
-        ['label' => 'Carrinho', 'url' => ['/carrinho/index']]
+    // Pages where the button should NOT appear
+    $excludedRoutes = [
+        'profile/index',
+        'site/login',
+        'site/signup',
+        'contact/index',
+        'carrinho/index',
     ];
 
-    if (!Yii::$app->user->isGuest)
-    {
-        $menuItems[] = ['label' => 'Faturas', 'url' => ['/fatura/index']];
-    }
+    $currentRoute = Yii::$app->controller->id . '/' . Yii::$app->controller->action->id;
 
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav me-auto mb-2 mb-md-0'],
-        'items' => $menuItems,
-    ]);
-
-    if (Yii::$app->user->isGuest)
-    {
-        echo Html::tag(
-            'div',Html::a('Login',['/site/login'],
-            ['class' => ['btn btn-link login text-dark text-decoration-none fa fa-user ']]),
-            ['class' => ['d-flex']]
-        );
-        echo Html::tag(
-            'div',Html::a('Signup',['/site/signup'],
-            ['class' => ['btn btn-link login text-dark text-decoration-none fa']]),
-            ['class' => ['d-flex']]
-        );
-    }
-    else
-    {
-       echo Html::a(
-           '<i class="fa fa-user"></i> Perfil',
-           ['/profile/index'],
-           ['class' => 'btn text-decoration-none'],
-       );
-        echo Html::beginForm(['/site/logout'], 'post', ['class' => 'd-flex'])
-            . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
-                ['class' => 'btn btn-link logout text-decoration-none text-dark']
-            )
-            . Html::endForm();
-    }
-    NavBar::end();
-    ?>
-</header>
-
-<main role="main" class="flex-shrink-0">
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= Alert::widget() ?>
-        <?= $content ?>
-    </div>
-</main>
-
-<!-- Footer Section Begin -->
-<footer class="footer spad">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3 col-md-6 col-sm-6">
-                <div class="footer__about">
-                    <div class="footer__about__logo">
-                        <a href="./index.html"><img src="img/logo.png" alt=""></a>
-                    </div>
-                    <ul>
-                        <li>Address: 60-49 Road 11378 New York</li>
-                        <li>Phone: +65 11.188.888</li>
-                        <li>Email: hello@colorlib.com</li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-6 col-sm-6 offset-lg-1">
-                <div class="footer__widget">
-                    <h6>Useful Links</h6>
-                    <ul>
-                        <li><a href="#">About Us</a></li>
-                        <li><a href="#">About Our Shop</a></li>
-                        <li><a href="#">Secure Shopping</a></li>
-                        <li><a href="#">Delivery infomation</a></li>
-                        <li><a href="#">Privacy Policy</a></li>
-                        <li><a href="#">Our Sitemap</a></li>
-                    </ul>
-                    <ul>
-                        <li><a href="#">Who We Are</a></li>
-                        <li><a href="#">Our Services</a></li>
-                        <li><a href="#">Projects</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li><a href="#">Innovation</a></li>
-                        <li><a href="#">Testimonials</a></li>
-                    </ul>
-                </div>
-            </div>
-            <div class="col-lg-4 col-md-12">
-                <div class="footer__widget">
-                    <h6>Join Our Newsletter Now</h6>
-                    <p>Get E-mail updates about our latest shop and special offers.</p>
-                    <form action="#">
-                        <input type="text" placeholder="Enter your mail">
-                        <button type="submit" class="site-btn">Subscribe</button>
-                    </form>
-                    <div class="footer__widget__social">
-                        <a href="#"><i class="fa fa-facebook"></i></a>
-                        <a href="#"><i class="fa fa-instagram"></i></a>
-                        <a href="#"><i class="fa fa-twitter"></i></a>
-                        <a href="#"><i class="fa fa-pinterest"></i></a>
-                    </div>
-                </div>
-            </div>
+    if (!in_array($currentRoute, $excludedRoutes)): ?>
+        <div class="floating-cart-button">
+            <?= Html::a(Html::tag('i', '', ['class' => 'fa fa-cart-shopping']), ['/carrinho/index'], ['class' => 'btn btn-primary']) ?>
         </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <div class="footer__copyright">
-                    <div class="footer__copyright__text"><p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-                            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p></div>
-                    <div class="footer__copyright__payment"><img src="img/payment-item.png" alt=""></div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <p class="float-start">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-        <p class="float-end"><?= Yii::powered() ?></p>
-    </div>
-</footer>
-<!-- Footer Section End -->
+    <?php endif; ?>
 
-<!-- Floating Cart Button -->
-<?php
-// Pages where the button should NOT appear
-$excludedRoutes = [
-    'profile/index',
-    'site/login',
-    'site/signup',
-    'contact/index',
-    'carrinho/index',
-];
-
-$currentRoute = Yii::$app->controller->id . '/' . Yii::$app->controller->action->id;
-
-if (!in_array($currentRoute, $excludedRoutes)): ?>
-    <div class="floating-cart-button">
-        <?= Html::a(Html::tag('i', '', ['class' => 'fa fa-cart-shopping']), ['/carrinho/index'], ['class' => 'btn btn-primary']) ?>
-    </div>
-<?php endif; ?>
-
-<?php $this->endBody() ?>
+    <?php $this->endBody() ?>
 </body>
+
 </html>
 <?php $this->endPage();
